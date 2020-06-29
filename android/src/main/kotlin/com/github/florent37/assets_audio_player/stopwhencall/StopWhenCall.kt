78168affc7c574c4f18959990221d322419c7ec0
Raghav Aggiwal <aggiwalraghav@gmail.com>
2020-06-29 20:35:08 +0530

    commit

---
+++ android/src/main/kotlin/com/github/florent37/assets_audio_player/stopwhencall/StopWhenCall.kt
@@ -0,0 +1,32 @@
+abstract class StopWhenCall {
+
+    enum class AudioState {
+        AUTHORIZED_TO_PLAY,
+        REDUCE_VOLUME,
+        FORBIDDEN
+    }
+
+    interface Listener {
+        fun onPhoneStateChanged(audioState: AudioState)
+    }
+
+    private val listeners = mutableSetOf<Listener>()
+
+    fun register(listener: Listener) {
+        listeners.add(listener)
+    }
+
+    fun unregister(listener: Listener) {
+        listeners.remove(listener)
+    }
+
+    protected fun pingListeners(audioState: AudioState) {
+        listeners.forEach {
+            it.onPhoneStateChanged(audioState= audioState)
+        }
+    }
+
+    abstract fun requestAudioFocus() : StopWhenCall.AudioState 
+    abstract fun stop()
+}
+
